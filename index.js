be142b551430ada468a942bab0b2879667ec968e
balili-melchorjr-developer <balili.melchorjr.developer@gmail.com>
2024-01-19 21:26:02 +0800

    Data types

--- index.js
+++ index.js
@@ -2,14 +2,39 @@
 
 var message = "Hello World"
 var age = 20
-
-
 let greeting = "Happy Birthday" // changeable values
-
-
 const PI = 3.14 // Unchangeable values
-
 
 
 // ----------------- Data Types --------------------
 
+// Primitive Data Types
+
+let messages = "Hello World" // String
+let ages = 10 // Number
+let isBoolean = true // Boolean
+
+// Reference Types 
+
+let person  = { // Objects
+    name: "John Doe",
+    age: 21,
+}
+
+// Two ways to access data in objects 
+// -- (person.name, person.age) dot notation
+// -- (person["name"], person["age"]) square bracket notation
+
+let fruits = [  // Arrays
+    "Apple",
+    "Orange",
+    "Durian"
+]
+
+// Access to array is index and also start with 0
+
+let add = function (a, b) { // Functions
+    return a + b
+}
+
+console.log(add(3, 5))
