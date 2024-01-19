f949177700adbf1ed7a263593d3b2a960f8c834b
balili-melchorjr-developer <balili.melchorjr.developer@gmail.com>
2024-01-19 21:08:26 +0800

    Data types

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hello World!')
+// ------------- Declaring Variables -------------
+
+var message = "Hello World"
+var age = 20
+
+
+let greeting = "Happy Birthday" // changeable values
+
+
+const PI = 3.14 // Unchangeable values
+
+
+
+// ----------------- Data Types --------------------
+
